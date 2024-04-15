bbf58ead8c693efafe68406722a4e38ed319030b
Ayham-Alhittini <ayham.hittini268@gmail.com>
2024-04-15 22:14:50 +0300

    track database collections changes(create, drop)

--- src/main/java/com/decentraldbcluster/dbclient/odm/database/EchoDatabase.java
+++ src/main/java/com/decentraldbcluster/dbclient/odm/database/EchoDatabase.java
@@ -1,8 +1,11 @@
 package com.decentraldbcluster.dbclient.odm.database;
 
+import com.decentraldbcluster.dbclient.DbClient;
+import com.decentraldbcluster.dbclient.Query;
 import com.decentraldbcluster.dbclient.annotaions.Id;
 import com.decentraldbcluster.dbclient.odm.collection.EchoCollection;
 import com.decentraldbcluster.dbclient.odm.collection.EchoCollectionFactory;
+import com.decentraldbcluster.dbclient.query.builders.CollectionQueryBuilder;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,6 +14,9 @@ import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Set;
 
 public abstract class EchoDatabase {
@@ -52,12 +58,66 @@ public abstract class EchoDatabase {
         throw new RuntimeException("Missing Id");
     }
     private void trackCollectionChanges() {
-//        if (!Files.exists(Path.of("src/main/resources/collectionStateTracker.ser"))) {
-//            for (Field field: this.getClass().getDeclaredFields()) {
-//
-//            }
-//        }
+        Set<String> currentCollectionsState = new HashSet<>();
+        for (Field field: this.getClass().getDeclaredFields())
+            currentCollectionsState.add(field.getName());
+
+        if (!Files.exists(Path.of("src/main/resources/collectionStateTracker.ser"))) {
+            createCollections(currentCollectionsState);
+        } else {
+            Set<String> previousCollectionState = deserialize();
+
+            Set<String> addedCollections = getAddedCollections(currentCollectionsState, previousCollectionState);
+            Set<String> removedCollections = getRemovedCollections(currentCollectionsState, previousCollectionState);
+
+            createCollections(addedCollections);
+            dropCollections(removedCollections);
+        }
+        serialize(currentCollectionsState);
     }
+
+    private Set<String> getAddedCollections(Set<String> currentCollectionsState, Set<String> previousCollectionState) {
+        Set<String> addedCollections = new HashSet<>();
+
+        for (String collection: currentCollectionsState) {
+            if (!previousCollectionState.contains(collection))
+                addedCollections.add(collection);
+        }
+        return addedCollections;
+    }
+
+    private Set<String> getRemovedCollections(Set<String> currentCollectionsState, Set<String> previousCollectionState) {
+        Set<String> removedCollections = new HashSet<>();
+
+        for (String collection: previousCollectionState) {
+            if (!currentCollectionsState.contains(collection))
+                removedCollections.add(collection);
+        }
+
+        return removedCollections;
+    }
+
+
+    private void createCollections(Set<String> collections) {
+        for (String collectionName: collections) {
+            Query query = new CollectionQueryBuilder()
+                    .createCollection(collectionName)
+                    .build();
+            DbClient client = DbClient.getInstance();
+            client.executeQuery(query);
+        }
+    }
+
+    private void dropCollections(Set<String> collections) {
+        for (String collectionName: collections) {
+            Query query = new CollectionQueryBuilder()
+                    .dropCollection(collectionName)
+                    .build();
+            DbClient client = DbClient.getInstance();
+            client.executeQuery(query);
+        }
+    }
+
 
     public static Set<String> deserialize() {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("src/main/resources/collectionStateTracker.ser"))) {
--- src/main/java/com/decentraldbcluster/dbclient/odm/helpers/ObjectMapperConfigurator.java
+++ src/main/java/com/decentraldbcluster/dbclient/odm/helpers/ObjectMapperConfigurator.java
@@ -1,12 +1,19 @@
 package com.decentraldbcluster.dbclient.odm.helpers;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectMapperConfigurator {
     public static ObjectMapper configureMapper() {
         ObjectMapper mapper = new ObjectMapper();
+
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
         return mapper;
     }
 }
